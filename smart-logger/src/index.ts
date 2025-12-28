67371fe3ccea02b26db36e00dc8ce48b66a84c6c
Mourya Vamsi <mouryamodugulamm@gmail.com>
2025-12-28 11:40:56 +0530

    added

---
+++ smart-logger/src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * @mouryavamsi/smart-logger
+ * A smart console logger with colored output, timestamps, and environment-aware logging
+ */
+
+export { log } from './logger.js';
+export type { Logger, LogLevel } from './types.js';
